33091f50d7dc464b385b9f56353458c7ea9748dc
ryukzzz12 <126538660+ryukzzz12@users.noreply.github.com>
2025-01-16 00:44:21 +0100

    Update ra.lua

--- ra.lua
+++ ra.lua
@@ -1,5 +1,7 @@
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/0xCiel/ScriptTest/refs/heads/main/Layer2Script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Yv50/Arox-v2/refs/heads/main/Arox%20v2-obfuscated.lua"))()
+
+
 wait(0.5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ryukzzz12/WAtermelonscriptroblox/refs/heads/main/w-a-t-e-r-m-e-l-o-n"))()
