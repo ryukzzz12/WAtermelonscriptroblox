b4203d7435852e4918142d6cb8481f89bc74b372
ryukzzz12 <126538660+ryukzzz12@users.noreply.github.com>
2025-01-16 00:43:59 +0100

    Update ra.lua

--- ra.lua
+++ ra.lua
@@ -1,1 +1,5 @@
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0xCiel/ScriptTest/refs/heads/main/Layer2Script.lua"))()
+wait(0.5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ryukzzz12/WAtermelonscriptroblox/refs/heads/main/w-a-t-e-r-m-e-l-o-n"))()
